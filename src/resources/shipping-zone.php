61ed658869b5a82efd960b33b38355c930328929
Andrew Cargill <cargix1@gmail.com>
2016-02-07 18:29:37 +0100

    Create shipping-zone.php

---
+++ src/resources/shipping-zone.php
@@ -0,0 +1,51 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Operations
+    |--------------------------------------------------------------------------
+    |
+    | This array of operations is translated into methods that complete these
+    | requests based on their configuration.
+    |
+    */
+
+    "operations" => array(
+
+        /**
+         *    getArticles() method
+         *
+         *    reference: http://docs.shopify.com/api/article
+         */
+        "getShippingZones" => array(
+            "httpMethod" => "GET",
+            "uri" => "/admin/shipping_zones.json",
+            "summary" => "Get a list of all shipping zones.",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "fields" => array(
+                    "type" => "string",
+                    "location" => "query",
+                    "description" => "comma-separated list of fields to include in the response."
+                )
+            )
+        ), 
+    ),
+    
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models
+    |--------------------------------------------------------------------------
+    |
+    | This array of models is specifications to returning the response
+    | from the operation methods.
+    |
+    */
+
+    "models" => array(
+
+    ),
+);
