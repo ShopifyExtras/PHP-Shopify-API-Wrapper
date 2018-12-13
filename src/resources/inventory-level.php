da14d9b93692601912fdaea9c9ca37bed2125501
Seb Maynard <seb@seb.so>
2018-12-13 22:44:52 +0000

    Added more inventory-level API methods

--- src/resources/inventory-level.php
+++ src/resources/inventory-level.php
@@ -22,7 +22,7 @@ return array(
         "getInventoryLevels" => array(
             "httpMethod" => "GET",
             "uri" => "/admin/inventory_levels.json",
-            "summary" => "Retrieve a list of all Gift Cards Adjustments for a Gift Card",
+            "summary" => "Retrieve a list of all inventory levels for the given inventory item ids",
             "responseModel" => "defaultJsonResponse",
             "parameters" => array(
                 "inventory_item_ids" => array(
@@ -34,6 +34,73 @@ return array(
                     "type" => "integer",
                     "location" => "query",
                     "description" => "Page to show (default: 1)"
+                ),
+            )
+        ),
+
+        "adjustInventoryLevels" => array(
+            "httpMethod" => "POST",
+            "uri" => "/admin/inventory_levels/adjust.json",
+            "summary" => "Adjust an inventory level",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "location_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the location id"
+                ),
+                "inventory_item_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the inventory item id"
+                ),
+                "available_adjustment" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the amount to change the inventory level by"
+                ),
+            )
+        ),
+
+        "setInventoryLevels" => array(
+            "httpMethod" => "POST",
+            "uri" => "/admin/inventory_levels/set.json",
+            "summary" => "set an inventory level",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "location_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the location id"
+                ),
+                "inventory_item_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the inventory item id"
+                ),
+                "available" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the amount to set the inventory level to"
+                ),
+            )
+        ),
+
+        "connectInventoryLevels" => array(
+            "httpMethod" => "POST",
+            "uri" => "/admin/inventory_levels/connect.json",
+            "summary" => "Connect a location to an inventory level",
+            "responseModel" => "defaultJsonResponse",
+            "parameters" => array(
+                "location_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the location id"
+                ),
+                "inventory_item_id" => array(
+                    "type" => "string",
+                    "location" => "json",
+                    "description" => "the inventory item id"
                 ),
             )
         ),
